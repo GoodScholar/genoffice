616fe142daff430c7c5d6ac6ccf1b334bcfa1924
Shen <1551397605@qq.com>
2026-09-23 21:48:15 +0800

    test(markdown): position save scenario caret portably

--- e2e/markdown-tab.spec.ts
+++ e2e/markdown-tab.spec.ts
@@ -123,8 +123,9 @@ test.describe('markdown editor', () => {
       await expect(editor.locator('strong')).toHaveText('bold')
 
       // type at the end of the document, save with ⌘/Ctrl+S
-      await editor.locator('p').last().click()
-      await editorPage.keyboard.press('End')
+      await editor.click()
+      await editorPage.keyboard.press('ControlOrMeta+a')
+      await editorPage.keyboard.press('ArrowRight')
       await editorPage.keyboard.press('Enter')
       await editorPage.keyboard.type('Appended line.')
       await editorPage.keyboard.press('ControlOrMeta+s')
